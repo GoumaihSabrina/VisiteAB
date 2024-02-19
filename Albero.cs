9a86da54d286c04b696f2736e3e728f1588a1770
GoumaihSabrina <9698228@itisrossi.vi.it>
2024-02-19 18:01:36 +0100

    bfs

---
+++ Albero.cs
@@ -0,0 +1,41 @@
+public class Albero{
+  public Node radice;
+  List<Node> coda = new List<Node>();//questa lista mi serve per la coda degli elementi
+   
+
+
+  public Albero(Node root){
+  this.radice = root;
+
+  }
+  public bool Bfs(Node nodo){//ricerca in ampiezza,nodi adiacenti
+   
+    if(radice == null){
+      return false;
+
+    }
+    
+   coda.Add(radice);
+
+while(coda.Count > 0){
+  Node nodoCorrente = coda[0];
+  coda.RemoveAt(0);//ho preso il primo elemento della coda 
+  if(nodoCorrente == nodo ){
+    return true;// solito controllo,se trovo il nodo che cerco ed é uguale a quello corrente appost
+  }  
+  if(nodoCorrente.nododx != null){
+    coda.Add(nodoCorrente.nododx);
+
+  }
+  if(nodoCorrente.nododx != null){
+    coda.Add(nodoCorrente.nodosx);
+  }
+
+
+
+  }
+  return false;// se non trovo nulla,return false 
+  }
+
+    
+  }
---
+++ Node.cs
@@ -0,0 +1,13 @@
+public class Node{
+    public Node nododx;
+    public Node nodosx;
+    public string val;
+
+    public Node(string val,Node nododx,Node nodosx){
+      this.val = val;
+      this.nodosx = nododx;
+      this.nododx = nododx;
+
+    }
+    
+}
